2b3e3a8604d172d17236697b25128308be4997b4
jgreen44 <ShallowDoF18!>
2022-12-27 14:30:16 -0600

    lint fix

--- src/util/types.ts
+++ src/util/types.ts
@@ -11,14 +11,11 @@ export type EventBodyType = {
 
 export const TABLE_NAME = 'ONE_TIME_PASSWORD';
 export const FROM_EMAIL = 'us291888@gmail.com';
-export const RETURN_PATH_ARN =
-  'arn:aws:ses:us-east-2:616233307424:identity/us291888@gmail.com';
+export const RETURN_PATH_ARN = 'arn:aws:ses:us-east-2:616233307424:identity/us291888@gmail.com';
 
-export const SOURCE_ARN =
-  'arn:aws:ses:us-east-2:616233307424:identity/us291888@gmail.com';
+export const SOURCE_ARN = 'arn:aws:ses:us-east-2:616233307424:identity/us291888@gmail.com';
 export const _headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
-  'Access-Control-Allow-Headers':
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 };
